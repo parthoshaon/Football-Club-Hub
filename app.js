e30a9d3ebecc6e46c40f9020453f83ed531b2809
Partho Shaon <parthoshaon@gmail.com>
2023-04-05 02:11:57 +0600

    La liga table and table css addded

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ expressSanitizer = require("express-sanitizer"),
 app = express();
 
 
+
 //app config
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
@@ -50,55 +51,136 @@ app.get("/", function(req, res){
 
 
 
-//Index Route
+// //Index Route
+// app.get("/blogs", function(req, res) {
+// 	// Fetch the next match information from the API
+// 	const axios = require('axios');
+// 	const apiKey = '262a8f5f0b7245a3a2742a448403349d';
+// 	const baseUrl = `https://api.football-data.org/v2/teams/81/matches?status=SCHEDULED`;
+// 	const tableUrl = `https://api.football-data.org/v2/competitions/2023/standings`;
+
+// 	//Add this code after the "const baseUrl" line but before the "axios.get" line in your app.get("/blogs"...) route handler
+// 	const tableUrl = `https://api.football-data.org/v2/competitions/2014/standings`;
+// 	axios.get(tableUrl, { headers: { 'X-Auth-Token': apiKey } })
+// 		.then(response => {
+// 			const tableData = response.data.standings[0].table;
+// 			res.render("index", {blogs: blogs, nextFixture: {
+// 				homeTeam: homeTeam,
+// 				awayTeam: awayTeam,
+// 				fixtureDate: fixtureDate,
+// 				fixtureTime: fixtureTime,
+// 				fixtureVenue: fixtureVenue
+// 			}, tableData: tableData});
+// 		})
+// 		.catch(error => {
+// 			console.error(error);
+// 			res.render("index", {blogs: blogs});
+// 		});
+
+  
+// 	axios.get(baseUrl, { headers: { 'X-Auth-Token': apiKey } })
+// 	  .then(response => {
+// 		// console.log(response.data);
+// 		const nextFixture = response.data.matches[0];
+// 		const homeTeam = nextFixture.homeTeam.name;
+// 		const awayTeam = nextFixture.awayTeam.name;
+// 		const fixtureDate = new Date(nextFixture.utcDate).toLocaleDateString();
+// 		const fixtureTime = new Date(nextFixture.utcDate).toLocaleTimeString();
+// 		const fixtureVenue = nextFixture.venue;
+
+// 		//console.log(homeTeam);
+  
+// 		// Render the index page with the blog posts and the next match information
+// 		Blog.find({}, function(err, blogs){
+// 		  if(err) {
+// 			console.log("ERRROR!");
+// 		  }
+// 		  else {
+// 			res.render("index", {blogs: blogs, nextFixture: {
+// 			  homeTeam: homeTeam,
+// 			  awayTeam: awayTeam,
+// 			  fixtureDate: fixtureDate,
+// 			  fixtureTime: fixtureTime,
+// 			  fixtureVenue: fixtureVenue
+// 			}});
+// 		  }
+// 		});
+// 	  })
+// 	  .catch(error => {
+// 		console.error(error);
+// 		// Render the index page with just the blog posts (without next match information)
+// 		Blog.find({}, function(err, blogs){
+// 		  if(err) {
+// 			console.log("ERRROR!");
+// 		  }
+// 		  else {
+// 			res.render("index", {blogs: blogs});
+// 		  }
+// 		});
+// 	  });
+//   });
+  
+
+
+
+
+
+
 app.get("/blogs", function(req, res) {
-	// Fetch the next match information from the API
 	const axios = require('axios');
 	const apiKey = '262a8f5f0b7245a3a2742a448403349d';
 	const baseUrl = `https://api.football-data.org/v2/teams/81/matches?status=SCHEDULED`;
+	const tableUrl = `https://api.football-data.org/v2/competitions/2014/standings`;
   
-	axios.get(baseUrl, { headers: { 'X-Auth-Token': apiKey } })
-	  .then(response => {
-		// console.log(response.data);
-		const nextFixture = response.data.matches[0];
-		const homeTeam = nextFixture.homeTeam.name;
-		const awayTeam = nextFixture.awayTeam.name;
-		const fixtureDate = new Date(nextFixture.utcDate).toLocaleDateString();
-		const fixtureTime = new Date(nextFixture.utcDate).toLocaleTimeString();
-		const fixtureVenue = nextFixture.venue;
-
-		//console.log(homeTeam);
+	Promise.all([
+	  axios.get(baseUrl, { headers: { 'X-Auth-Token': apiKey } }),
+	  axios.get(tableUrl, { headers: { 'X-Auth-Token': apiKey } })
+	])
+	.then(responseArr => {
+	  const nextFixture = responseArr[0].data.matches[0];
+	  const homeTeam = nextFixture.homeTeam.name;
+	  const awayTeam = nextFixture.awayTeam.name;
+	  const fixtureDate = new Date(nextFixture.utcDate).toLocaleDateString();
+	  const fixtureTime = new Date(nextFixture.utcDate).toLocaleTimeString();
+	  const fixtureVenue = nextFixture.venue;
   
-		// Render the index page with the blog posts and the next match information
-		Blog.find({}, function(err, blogs){
-		  if(err) {
-			console.log("ERRROR!");
-		  }
-		  else {
-			res.render("index", {blogs: blogs, nextFixture: {
+	  const tableData = responseArr[1].data.standings[0].table;
+  
+	  Blog.find({}, function(err, blogs) {
+		if (err) {
+		  console.log("ERROR!");
+		} else {
+		  res.render("index", {
+			blogs: blogs,
+			nextFixture: {
 			  homeTeam: homeTeam,
 			  awayTeam: awayTeam,
 			  fixtureDate: fixtureDate,
 			  fixtureTime: fixtureTime,
 			  fixtureVenue: fixtureVenue
-			}});
-		  }
-		});
-	  })
-	  .catch(error => {
-		console.error(error);
-		// Render the index page with just the blog posts (without next match information)
-		Blog.find({}, function(err, blogs){
-		  if(err) {
-			console.log("ERRROR!");
-		  }
-		  else {
-			res.render("index", {blogs: blogs});
-		  }
-		});
+			},
+			tableData: tableData
+		  });
+		}
 	  });
+	})
+	.catch(error => {
+	  console.error(error);
+	  Blog.find({}, function(err, blogs) {
+		if (err) {
+		  console.log("ERROR!");
+		} else {
+		  res.render("index", { blogs: blogs });
+		}
+	  });
+	});
   });
   
+
+
+
+
+
 
 //Create Route
 app.post("/blogs", function(req, res){
